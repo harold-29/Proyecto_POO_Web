17bb0fa22eb8e17ebeb4bde998fcb428d8d77e5d
harold-29 <haroldbaduma@gmail.com>
2023-07-13 22:15:41 -0600

    13/07/23 22:15 Validaciones en el registro de cliente

--- Fase3_POO/frmRegistro.aspx
+++ Fase3_POO/frmRegistro.aspx
@@ -5,28 +5,36 @@
 <asp:Content ID="Content2" ContentPlaceHolderID="main" runat="server">
     <div class="container bd-main col">
         <h2 class="h2">Ingresa tus datos</h2>
+
         <form runat="server">
             <div class="row row-cols-1 mt-5 row-cols-md-3">
 
                 <div class="col mt-2">
                     <p>Nombre</p>
                     <asp:TextBox ID="txtNombre" runat="server" MaxLength="50" CssClass="form-control"></asp:TextBox>
+                    <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ErrorMessage='El campo "Nombre" es requerido' ControlToValidate="txtNombre" Text="*" ValidationGroup="Registro"></asp:RequiredFieldValidator>
                 </div>
 
                 <div class="col mt-2">
 
                     <p>Primer Apellido</p>
                     <asp:TextBox ID="txtApellido1" runat="server" MaxLength="50" CssClass="form-control"></asp:TextBox>
+                    <asp:RequiredFieldValidator ID="RequiredFieldValidator2" runat="server" ErrorMessage='El campo "Primer Apellido" es requerido' ControlToValidate="txtApellido1" Text="*" ValidationGroup="Registro"></asp:RequiredFieldValidator>
+
                 </div>
 
                 <div class="col mt-2">
                     <p>Segundo Apellido</p>
                     <asp:TextBox ID="txtApellido2" runat="server" MaxLength="50" CssClass="form-control"></asp:TextBox>
+                    <asp:RequiredFieldValidator ID="RequiredFieldValidator3" runat="server" ErrorMessage='El campo "Segundo Apellido" es requerido' ControlToValidate="txtApellido2" Text="*" ValidationGroup="Registro"></asp:RequiredFieldValidator>
+
                 </div>
 
                 <div class="col mt-2">
                     <p>Identificación</p>
                     <asp:TextBox ID="txtID" runat="server" MaxLength="15" CssClass="form-control"></asp:TextBox>
+                    <asp:RequiredFieldValidator ID="RequiredFieldValidator4" runat="server" ErrorMessage='El campo "Identificación" es requerido' ControlToValidate="txtID" Text="*" ValidationGroup="Registro"></asp:RequiredFieldValidator>
+
                 </div>
 
                 <div class="col mt-2">
@@ -43,11 +51,14 @@
                 <div class="col mt-2">
                     <p>Provincia</p>
                     <asp:DropDownList ID="ddlProvincia" runat="server" class="form-control"></asp:DropDownList>
+                    <asp:RequiredFieldValidator ID="RequiredFieldValidator8" runat="server" ErrorMessage='El campo "Provincia" es requerido' ControlToValidate="ddlProvincia" Text="*" ValidationGroup="Registro"></asp:RequiredFieldValidator>
 
                 </div>
                 <div class="col mt-2">
                     <p>Cantón</p>
                     <asp:TextBox ID="txtCanton" runat="server" MaxLength="50" CssClass="form-control"></asp:TextBox>
+                    <asp:RequiredFieldValidator ID="RequiredFieldValidator5" runat="server" ErrorMessage='El campo "Cantón" es requerido' ControlToValidate="txtCanton" Text="*" ValidationGroup="Registro"></asp:RequiredFieldValidator>
+
                 </div>
 
                 <div class="col mt-2">
@@ -57,6 +68,7 @@
                     <span id="datetimepicker" class="input-group-addon">
                         <span class="glyphicon glyphicon-calendar"></span>
                     </span>
+                    <asp:RequiredFieldValidator ID="RequiredFieldValidator6" runat="server" ErrorMessage='El campo "Fecha de Nacimiento" es requerido' ControlToValidate="txtFechaNacimiento" Text="*" ValidationGroup="Registro"></asp:RequiredFieldValidator>
 
                 </div>
 
@@ -66,10 +78,17 @@
                 <div class="col">
                     <p>Dirección</p>
                     <textarea id="txtDireccion" cols="20" rows="2" runat="server" class="form-control"></textarea>
+                    <asp:RequiredFieldValidator ID="RequiredFieldValidator7" runat="server" ErrorMessage='El campo "Dirección" es requerido' ControlToValidate="txtDireccion" Text="*" ValidationGroup="Registro"></asp:RequiredFieldValidator>
+
                 </div>
 
                 <div class="col my-5 ">
-                    <asp:Button ID="btnRegistrar" runat="server" Text="Registrar" class="btn btn-warning" OnClick="btnRegistrar_Click" />
+                    <asp:Button ID="btnRegistrar" runat="server" Text="Registrar" class="btn btn-warning" OnClick="btnRegistrar_Click" ValidationGroup="Registro" />
+                </div>
+
+                <div class="col my-5 ">
+                    <asp:ValidationSummary ID="ValidationSummary1" runat="server" ValidationGroup="Registro" />
+
                 </div>
             </div>
 
--- Fase3_POO/frmRegistro.aspx.designer.cs
+++ Fase3_POO/frmRegistro.aspx.designer.cs
@@ -24,6 +24,15 @@ namespace Fase3_POO
         protected global::System.Web.UI.WebControls.TextBox txtNombre;
 
         /// <summary>
+        /// Control RequiredFieldValidator1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator1;
+
+        /// <summary>
         /// Control txtApellido1.
         /// </summary>
         /// <remarks>
@@ -31,6 +40,15 @@ namespace Fase3_POO
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox txtApellido1;
+
+        /// <summary>
+        /// Control RequiredFieldValidator2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator2;
 
         /// <summary>
         /// Control txtApellido2.
@@ -42,6 +60,15 @@ namespace Fase3_POO
         protected global::System.Web.UI.WebControls.TextBox txtApellido2;
 
         /// <summary>
+        /// Control RequiredFieldValidator3.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator3;
+
+        /// <summary>
         /// Control txtID.
         /// </summary>
         /// <remarks>
@@ -49,6 +76,15 @@ namespace Fase3_POO
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox txtID;
+
+        /// <summary>
+        /// Control RequiredFieldValidator4.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator4;
 
         /// <summary>
         /// Control txtEmail.
@@ -78,6 +114,15 @@ namespace Fase3_POO
         protected global::System.Web.UI.WebControls.DropDownList ddlProvincia;
 
         /// <summary>
+        /// Control RequiredFieldValidator8.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator8;
+
+        /// <summary>
         /// Control txtCanton.
         /// </summary>
         /// <remarks>
@@ -85,6 +130,15 @@ namespace Fase3_POO
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox txtCanton;
+
+        /// <summary>
+        /// Control RequiredFieldValidator5.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator5;
 
         /// <summary>
         /// Control txtFechaNacimiento.
@@ -96,6 +150,15 @@ namespace Fase3_POO
         protected global::System.Web.UI.WebControls.TextBox txtFechaNacimiento;
 
         /// <summary>
+        /// Control RequiredFieldValidator6.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator6;
+
+        /// <summary>
         /// Control txtDireccion.
         /// </summary>
         /// <remarks>
@@ -105,6 +168,15 @@ namespace Fase3_POO
         protected global::System.Web.UI.HtmlControls.HtmlTextArea txtDireccion;
 
         /// <summary>
+        /// Control RequiredFieldValidator7.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator7;
+
+        /// <summary>
         /// Control btnRegistrar.
         /// </summary>
         /// <remarks>
@@ -112,5 +184,14 @@ namespace Fase3_POO
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btnRegistrar;
+
+        /// <summary>
+        /// Control ValidationSummary1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ValidationSummary ValidationSummary1;
     }
 }
